862d7375119419ded96fe6e51b6a08e609f6843b
Ripudaman Singh <ripu502@gmail.com>
2020-06-23 21:23:47 +0530

    saving to josn

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
 const router = require('./routers/router');
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+
 app.use(router);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
 })
+server.timeout = 60000;
--- controller/ballHandler.js
+++ controller/ballHandler.js
@@ -1,9 +1,52 @@
+const g = 10;
+
+
 module.exports.getData = (req, res, next) => {
-    res.send('hello')
+    res.status(200).json(
+        {
+            response: 'data from the database'
+        });
+}
+
+
+module.exports.makePredict = (req, res, next) => {
+    let { height, e } = req.body;
+    let arr = [{ 'height': height, 'time': 0 }];
+    let t = 0;
+    let bounce = 0;
+    while (height > 0) {
+        if (e >= 1) {
+            res.status(200).json(
+                {
+                    response: 'bad value for the e'
+                });
+            break;
+        }
+        let v = Math.sqrt(2 * g * height);
+        t = t + parseFloat((v / g).toFixed(1));
+        let temp = { 'height': 0, 'time': t };
+        arr.push(temp);
+        height = (e * e * height).toFixed(0);
+        if (height == 0) break;
+        t = t + parseFloat((e * v / g).toFixed(1));
+        bounce++;
+        temp = { 'height': height, 'time': t };
+        arr.push(temp);
+    }
+
+    res.status(200).json(
+        {
+            response: 'done calculation',
+            bounce: bounce,
+            data: arr
+        })
 }
 
 
 // add Bad Request
 module.exports.badReq = (req, res, next) => {
-    res.send('bad req')
+    res.status(404).json(
+        {
+            response: 'bad request'
+        });
 }
