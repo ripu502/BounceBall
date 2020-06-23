610e42327574487ad652ef9491da5987b6ddff7a
Ripudaman Singh <ripu502@gmail.com>
2020-06-23 23:47:39 +0530

    all done

--- controller/ballHandler.js
+++ controller/ballHandler.js
@@ -2,20 +2,6 @@ const g = 10;
 const fs = require("fs");
 const dataPath = './data/data.json'
 
-const readFile = (
-    callback,
-    returnJson = false,
-    filePath = dataPath,
-    encoding = "utf8"
-) => {
-    fs.readFile(filePath, encoding, (err, data) => {
-        if (err) {
-            throw err;
-        }
-        callback(returnJson ? JSON.parse(data) : data);
-    });
-};
-
 const writeFile = (
     fileData,
     callback,
